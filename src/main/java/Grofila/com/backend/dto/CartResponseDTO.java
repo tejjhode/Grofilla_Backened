1eb3dfaae1c5e172276e13d1a90a2dd3e94c2ce2
Tejaswa Jhode <jhodetejswa2004@gmail.com>
2025-03-15 16:10:30 +0530

    working

--- src/main/java/Grofila/com/backend/dto/CartResponseDTO.java
+++ src/main/java/Grofila/com/backend/dto/CartResponseDTO.java
@@ -3,13 +3,12 @@ package Grofila.com.backend.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Data
-@AllArgsConstructor
+
 @NoArgsConstructor
 public class CartResponseDTO {
     @Id
