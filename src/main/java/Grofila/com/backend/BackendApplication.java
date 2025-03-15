86d7fe74f0c5c597b1e14cdf5d7afdec7c511eb5
Tejaswa Jhode <jhodetejswa2004@gmail.com>
2025-03-15 15:02:56 +0530

    working

--- src/main/java/Grofila/com/backend/BackendApplication.java
+++ src/main/java/Grofila/com/backend/BackendApplication.java
@@ -1,5 +1,4 @@
-package Grofila.com.backend;
-
+package grofila.com.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
